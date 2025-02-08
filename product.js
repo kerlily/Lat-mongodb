393e3165f1aa3b78ee843946fc360b63ea833f5e
kerlily <farhan15062004@gmail.com>
2025-02-08 12:57:43 +0700

    menggunakan product schema untuk function update indiivud atau all

--- product.js
+++ product.js
@@ -54,13 +54,38 @@ const productSchema = new mongoose.Schema({
             required: true
         }
     }
-
-
-
-
 })
 
+productSchema.methods.outStock = function () {
+    this.stock = 0
+    return this.save()
+}
+
+productSchema.statics.closeStore = function () {
+    return this.updateMany({}, {
+        stock: 0,
+        "availability.offline": false,
+        "availability.online": false,
+        price: 0
+
+    }
+    )
+};
 const Product = mongoose.model('Product', productSchema);
+
+const changeStock = async (id) => {
+    const foundProduct = await Product.findById(id)
+    await foundProduct.outStock()
+    console.log('berhasil kang');
+}
+
+Product.closeStore().then((result) => {
+    console.log(result);
+}).catch((err) => {
+    consoole.log(err);
+});
+
+// changeStock('679e6326a55c02f035e5396d')
 
 // const shirt = new Product({                               
 //     "name": "Celana Chino",
@@ -83,23 +108,23 @@ const Product = mongoose.model('Product', productSchema);
 //     console.log(err);
 // })
 
-Product.findOneAndUpdate(
-    {name: "Celana Chino"},{
-    "name": "Celana Chino",
-    "brand": "Levi's",
-    "price": 80000,
-    "color": "krem",
-    "size": ["28", "30", "32", "34", "36"],
-    "description": "Celana chino dengan warna yang cerah",
-    "condition": "baru",
-    "stock": 5,
-    "availability": {
-        "online": true,
-        "offline": true
-    }
-    },{new: true, runValidators: true}
-).then((result) => {
-    console.log(result);
-}).catch((err) => {
-    console.log(err);
-})
+// Product.findOneAndUpdate(
+//     {name: "Celana Chino"},{
+//     "name": "Celana Chino",
+//     "brand": "Levi's",
+//     "price": 80000,
+//     "color": "krem",
+//     "size": ["28", "30", "32", "34", "36"],
+//     "description": "Celana chino dengan warna yang cerah",
+//     "condition": "baru",
+//     "stock": 5,
+//     "availability": {
+//         "online": true,
+//         "offline": true
+//     }
+//     },{new: true, runValidators: true}
+// ).then((result) => {
+//     console.log(result);
+// }).catch((err) => {
+//     console.log(err);
+// })
